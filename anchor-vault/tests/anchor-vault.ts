e121b9f0d7bdf7789e6649ca2b9d6fda0bc1b596
AvhiMaz <avhimazumder5@outlook.com>
2025-04-09 19:19:50 +0530

    delete
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- anchor-vault/tests/anchor-vault.ts
+++ anchor-vault/tests/anchor-vault.ts
@@ -14,146 +14,137 @@ describe("anchor-vault", () => {
 
   before(async () => {
     const tx = await provider.connection.requestAirdrop(
-     signer.publicKey,
+      signer.publicKey,
       1_000_000_000
     );
 
     await provider.connection.confirmTransaction(tx, "confirmed");
 
     const signerBal = await provider.connection.getBalance(signer.publicKey);
-  })
+  });
 
   it("init", async () => {
-
     const vaultState = PublicKey.findProgramAddressSync(
-    [Buffer.from("state"), signer.publicKey.toBuffer()],
-    program.programId
-  )[0];
+      [Buffer.from("state"), signer.publicKey.toBuffer()],
+      program.programId
+    )[0];
 
-  const vault = PublicKey.findProgramAddressSync(
-    [Buffer.from("vault"), vaultState.toBuffer()],
-    program.programId
-  )[0];
+    const vault = PublicKey.findProgramAddressSync(
+      [Buffer.from("vault"), vaultState.toBuffer()],
+      program.programId
+    )[0];
 
-  try {
-    const tx = await program.methods.init()
-      .accounts({
-        signer: signer.publicKey,
-        vaultState,
-        vault,
-        systemProgram: SystemProgram.programId
-      })
-      .signers([signer])
-      .rpc();
+    try {
+      const tx = await program.methods
+        .init()
+        .accounts({
+          signer: signer.publicKey,
+          vaultState,
+          vault,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([signer])
+        .rpc();
       console.log("transaction signature", tx);
-
-    } catch (error){
-    console.log("error", error);
+    } catch (error) {
+      console.log("error", error);
     }
   });
 
   it("deposit", async () => {
-
     const vaultState = PublicKey.findProgramAddressSync(
-    [Buffer.from("state"), signer.publicKey.toBuffer()],
-    program.programId
+      [Buffer.from("state"), signer.publicKey.toBuffer()],
+      program.programId
     )[0];
 
     const vault = PublicKey.findProgramAddressSync(
-    [Buffer.from("vault"), vaultState.toBuffer()],
-    program.programId
+      [Buffer.from("vault"), vaultState.toBuffer()],
+      program.programId
     )[0];
 
-    const tx = await provider.connection.requestAirdrop(
-     vault,
-      1_000_000_000
-    );
+    const tx = await provider.connection.requestAirdrop(vault, 1_000_000_000);
 
     await provider.connection.confirmTransaction(tx, "confirmed");
 
     try {
-      const tx = await program.methods.deposit(new anchor.BN(1000))
-      .accounts({
-        signer: signer.publicKey,
-        vaultState,
-        vault,
-        systemProgram: SystemProgram.programId
-    })
-    .signers([signer])
-    .rpc();
+      const tx = await program.methods
+        .deposit(new anchor.BN(1000))
+        .accounts({
+          signer: signer.publicKey,
+          vaultState,
+          vault,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([signer])
+        .rpc();
 
-    console.log("transaction signature", tx);
-
-    } catch (error){
-      console.log("error", error)
+      console.log("transaction signature", tx);
+    } catch (error) {
+      console.log("error", error);
     }
-  })
+  });
 
   it("withdraw", async () => {
-
     const vaultState = PublicKey.findProgramAddressSync(
-    [Buffer.from("state"), signer.publicKey.toBuffer()],
-    program.programId
+      [Buffer.from("state"), signer.publicKey.toBuffer()],
+      program.programId
     )[0];
 
     const vault = PublicKey.findProgramAddressSync(
-    [Buffer.from("vault"), vaultState.toBuffer()],
-    program.programId
+      [Buffer.from("vault"), vaultState.toBuffer()],
+      program.programId
     )[0];
 
     let balanebefore = await provider.connection.getBalance(vault);
     console.log(balanebefore);
 
     try {
-      const tx = await program.methods.withdraw(new anchor.BN(1000))
-      .accounts({
-        signer: signer.publicKey,
-        vaultState,
-        vault,
-        systemProgram: SystemProgram.programId
-    })
-    .signers([signer])
-    .rpc();
+      const tx = await program.methods
+        .withdraw(new anchor.BN(1000))
+        .accounts({
+          signer: signer.publicKey,
+          vaultState,
+          vault,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([signer])
+        .rpc();
 
-    let balanceafter= await provider.connection.getBalance(vault);
-    console.log(balanceafter);
+      let balanceafter = await provider.connection.getBalance(vault);
+      console.log(balanceafter);
 
-
-    console.log("transaction signature", tx);
-
-    } catch (error){
-      console.log("error", error)
+      console.log("transaction signature", tx);
+    } catch (error) {
+      console.log("error", error);
     }
-  })
+  });
 
   it("close", async () => {
-
     const vaultState = PublicKey.findProgramAddressSync(
-    [Buffer.from("state"), signer.publicKey.toBuffer()],
-    program.programId
+      [Buffer.from("state"), signer.publicKey.toBuffer()],
+      program.programId
     )[0];
 
     const vault = PublicKey.findProgramAddressSync(
-    [Buffer.from("vault"), vaultState.toBuffer()],
-    program.programId
+      [Buffer.from("vault"), vaultState.toBuffer()],
+      program.programId
     )[0];
 
-
     try {
-      const tx = await program.methods.close()
-      .accounts({
-        signer: signer.publicKey,
-        vaultState,
-        vault,
-        systemProgram: SystemProgram.programId
-    })
-    .signers([signer])
-    .rpc();
+      const tx = await program.methods
+        .close()
+        .accounts({
+          signer: signer.publicKey,
+          vaultState,
+          vault,
+          systemProgram: SystemProgram.programId,
+        })
+        .signers([signer])
+        .rpc();
 
-    console.log("transaction signature", tx);
-
-    } catch (error){
-      console.log("error", error)
+      console.log("transaction signature", tx);
+    } catch (error) {
+      console.log("error", error);
     }
-  })
+  });
 });
