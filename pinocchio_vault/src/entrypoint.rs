5f78902f179b4e498bd4194f126aa39e0a343cb6
AvhiMaz <avhimazumder5@outlook.com>
2025-04-10 08:51:46 +0530

    update lib, entrypoint (pinocchio)
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- pinocchio_vault/src/entrypoint.rs
+++ pinocchio_vault/src/entrypoint.rs
@@ -1,29 +1,36 @@
-use crate::instruction::{self, VaultInstruction};
+#![allow(unexpected_cfgs)]
 use pinocchio::{
     account_info::AccountInfo, no_allocator, nostd_panic_handler, program_entrypoint,
-    pubkey::Pubkey, ProgramError, ProgramResult,
+    program_error::ProgramError, pubkey::Pubkey, ProgramResult,
 };
 use pinocchio_log::log;
 
-entrypoint!(process_instruction);
+use crate::instructions::{self, VaultInstruction};
+
+program_entrypoint!(process_instruction);
+//Do not allocate memory.
+no_allocator!();
+// Use the no_std panic handler.
+#[cfg(target_os = "solana")]
+nostd_panic_handler!();
 
 pub fn process_instruction(
-    program_id: &Pubkey,
+    _program_id: &Pubkey,
     accounts: &[AccountInfo],
     instruction_data: &[u8],
 ) -> ProgramResult {
     let (discriminator_variant, instruction_data) = instruction_data
         .split_first()
-        .ok_or(ProgramError::InvalidInstructionData);
+        .ok_or(ProgramError::InvalidInstructionData)?;
 
     match VaultInstruction::try_from(discriminator_variant)? {
         VaultInstruction::Deposit => {
             log!("Ix:0");
-            instruction::process_deposit(accounts, instruction_data);
+            instructions::process_deposit(accounts, instruction_data)?;
         }
-        VaultInstruction::withdraw => {
+        VaultInstruction::Withdraw => {
             log!("Ix:1");
-            instruction::process_withdraw(account, instruction_data);
+            instructions::process_withdraw(accounts, instruction_data)?;
         }
     }
 
--- pinocchio_vault/src/lib.rs
+++ pinocchio_vault/src/lib.rs
@@ -9,3 +9,5 @@ extern crate std;
 pub mod errors;
 pub mod instructions;
 pub mod states;
+
+pinocchio_pubkey::declare_id!("Hsj2dmft6MSJmj8VoaW7hWp9h3G4qf4B58LFfz5amPUz");
