d6120ff375705e75d0eedb3eaeb064953baf6b81
AvhiMaz <avhimazumder5@outlook.com>
2025-04-10 12:11:17 +0530

    withdraw test (pinocchio)
    Signed-off-by: AvhiMaz <avhimazumder5@outlook.com>

--- pinocchio_vault/tests/unit_tests.rs
+++ pinocchio_vault/tests/unit_tests.rs
@@ -58,3 +58,41 @@ fn test_deposit() {
 
     assert!(init_res.program_result == ProgramResult::Success);
 }
+
+#[test]
+fn test_withdraw() {
+    let mollusk = mollusk();
+
+    let (system_prgram, system_account) = program::keyed_account_for_system_program();
+
+    let (vault_pda, bump) = Pubkey::find_program_address(
+        &["pinocchio_vault_pda".as_bytes(), &PAYER.to_bytes()],
+        &PROGRAM,
+    );
+
+    let payer_acc = Account::new(9, 0, &system_prgram);
+    let vault_acc = Account::new(1, 0, &system_prgram);
+
+    let ix_account = vec![
+        AccountMeta::new(PAYER, true),
+        AccountMeta::new(vault_pda, false),
+        AccountMeta::new(system_prgram, false),
+    ];
+
+    let mut ix_data = vec![1];
+
+    ix_data.push(bump);
+
+    let instruction = Instruction::new_with_bytes(PROGRAM, &ix_data, ix_account);
+
+    let tx_accounts = &vec![
+        (PAYER, payer_acc.clone()),
+        (vault_pda, vault_acc.clone()),
+        (system_prgram, system_account.clone()),
+    ];
+
+    let update_res =
+        mollusk.process_and_validate_instruction(&instruction, tx_accounts, &[Check::success()]);
+
+    assert!(update_res.program_result == ProgramResult::Success);
+}
